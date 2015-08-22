620108694e1088bdc5994b4fbf724a035534415b
Marc O'Morain <marc@circleci.com>
2015-08-22 10:43:22 +0100

    Prepare for release

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "http://github.com/circleci/rollcage"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :global-vars  {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.4.0"]
                  [clj-http "2.0.0"]
