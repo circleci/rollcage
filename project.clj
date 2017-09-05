6df8e9e9ab318436cba28a4414e8ffa5f6f02e4f
Marc O'Morain <github.com@marcomorain.com>
2017-09-05 11:51:07 +0100

    Update deployment

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://github.com/circleci/rollcage"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :deploy-repositories ^:replace [["clojars" {:url "https://clojars.org/repo"
-                                              :username [:gpg :env/clojars_username]
-                                              :password [:gpg :env/clojars_password]
-                                              :sign-releases false}]
-                                  ["releases" :clojars]]
+  :repositories [["clojars" {:url "https://clojars.org/repo"
+                             :username [:gpg :env/clojars_username]
+                             :password [:gpg :env/clojars_password]
+                             :sign-releases false}]
+                 ["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.4.0"]
                  [clj-http "2.0.0"]
