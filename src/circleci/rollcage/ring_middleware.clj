25e88316ad155cd41a65c374011b858eeca9de4e
Jérémy Van de Wyngaert <jeremyvdw@protonmail.com>
2017-11-22 09:46:32 +0100

    select keys to be sent by ring-middleware

--- src/circleci/rollcage/ring_middleware.clj
+++ src/circleci/rollcage/ring_middleware.clj
@@ -1,6 +1,10 @@
 (ns circleci.rollcage.ring-middleware
   (:require [circleci.rollcage.core :as rollcage]))
 
+(def ^:const request-and-wrap-parameter-keys
+  "selector for keys provided by ring's request map and by ring's wrap-parameter middleware"
+  [:http-method :query-string :query-params :form-params :params])
+
 (defn wrap-rollbar [handler rollcage-client]
   (if-not rollcage-client
     handler
@@ -9,5 +13,5 @@
         (handler req)
         (catch Exception e
           (rollcage/error rollcage-client e {:url (:uri req)
-                                             :params (dissoc req :uri)})
+                                             :params (select-keys req request-and-wrap-parameter-keys)})
           (throw e))))))
